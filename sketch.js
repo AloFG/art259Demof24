001593b62a277a054080b3129777b4a9d3e92bc1
AloFG <garciaag0707@gmail.com>
2024-10-29 17:23:52 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,9 @@
 function setup() {
-  createCanvas(400, 400);
+  let canvas = createCanvas(400, 400);
   canvas.parent("project");
 }
 
 function draw() {
+  background(100);
   ellipse(mouseX, mouseY, 25);
 }
-
-// function draw() {
-//   background(0);
-
-//   lollipop(200, 200, 100, 150,);
-//   lollipop(400, 100, 250, 300);
-//   lollipop(50, 100, 50, 100);
-// }
-
-// function lollipop(x, y, lolliDiameter, stickLength,){
-//   fill(0, 200, 255);
-//   rect(x-10, y, 20, stickLength);
-
-//   fill(255, 0, 200);
-//   ellipse(x, y, lolliDiameter, lolliDiameter);
-// }
